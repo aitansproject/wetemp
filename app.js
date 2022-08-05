55628f126b4966012c0151dbee80bf970ea33051
end4project <aytennadirli1@mail.ru>
2022-08-05 11:14:29 +0000

    about

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&units=metric`
+            let url = 
             async function myweather() {
                 let responce = await fetch(url)
-                let data = await responce.json()
-                $("#city").html(data.name)
-                $("#temp").html(data.main.temp)
-                $("#feels").html(data.main.feels_like)
+                let textbox = await responce.json()
+                $("#city").html(textbox.name)
+                $("#temperature").html(textbox.main.temperature)
+                $("#feels").html(textbox.main.feels_like)
                 $("#myWeather").val("")
-                console.log(data)
+                console.log(textbox)
             }
             myweather()
         }
@@ -19,19 +19,18 @@ $(document).ready(function () {
 
     $("#btn").click(function (e) {
         e.preventDefault()
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&units=metric`
+        let url = 
         async function myweather() {
             let responce = await fetch(url)
-            let data = await responce.json()
-            $("#city").html(data.name)
-            $("#temp").html(data.main.temp)
-            $("#feels").html(data.main.feels_like)
+            let textbox = await responce.json()
+            $("#city").html(textbox.name)
+            $("#temperature").html(textbox.main.temperature)
+            $("#feels").html(textbox.main.feels_like)
             $("#myWeather").val("")
             console.log(data)
         }
         myweather()
     })
-
 
 
 });
