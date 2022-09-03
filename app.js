40498c35d6aa0e46152280a540356fa68d993ee7
end4project <aytennadirli1@mail.ru>
2022-09-03 20:09:50 +0000

    about

--- app.js
+++ app.js
@@ -3,33 +3,33 @@ $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q={${$("#myweather").val()}&appid={API key}`
-            async function myweather() {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=9464e69222db2063be4596adb4418268=metric`
+            async function myWeather() {
                 let responce = await fetch(url)
                 let data = await responce.json()
                 $("#city").html(data.name)
-                $("#temperature").html(data.main.temperature)
+                $("#temp").html(data.main.temp)
                 $("#feels").html(data.main.feels_like)
                 $("#myWeather").val("")
                 console.log(data)
             }
-            myweather()
+            myWeather()
         }
     })
 
     $("#btn").click(function (e) {
         e.preventDefault()
-        let url = `https://api.openweathermap.org/data/2.5/weather?q={${$("#myweather").val()}&appid={API key}`
-        async function myweather() {
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=9464e69222db2063be4596adb4418268=metric`
+        async function myWeather() {
             let responce = await fetch(url)
             let data = await responce.json()
             $("#city").html(data.name)
-            $("#temperature").html(data.main.temperature)
+            $("#temp").html(data.main.temp)
             $("#feels").html(data.main.feels_like)
             $("#myWeather").val("")
             console.log(data)
         }
-        myweather()
+        myWeather()
     })
 
 });
